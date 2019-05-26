87995185d16ae6c67a9acb93dd2afafc9151e942
KozlovaDasha <dashakozlova255@ukr.net>
2019-05-26 23:31:37 +0300

    Добавьте файлы проекта.

---
+++ Pr5.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            A First = new A(12,34);
+            Console.WriteLine(First.C);
+            Console.ReadLine();
+        }
+    }
+}
